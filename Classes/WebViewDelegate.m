11c47ec2792c75d7b87fec0a99b01c09d995655c
Alex MacCaw <maccman@gmail.com>
2012-01-07 08:59:27 -0800

    Make sure context menu does not show refresh

--- Classes/WebViewDelegate.m
+++ Classes/WebViewDelegate.m
@@ -17,7 +17,7 @@
 @synthesize dock;
 @synthesize growl;
 
-- (void) webView:(WebView*)webView windowScriptObjectAvailable:(WebScriptObject*)windowScriptObject
+- (void) webView:(WebView*)webView didClearWindowObject:(WebScriptObject*)windowScriptObject forFrame:(WebFrame *)frame
 {
 	if (self.sound == nil) { self.sound = [Sound new]; }
 	[windowScriptObject setValue:self.sound forKey:@"sound"];
@@ -29,28 +29,59 @@
 	[windowScriptObject setValue:self.growl forKey:@"growl"];
 }
 
-/* This logs all errors from Javascript, nifty */
 - (void) webView:(WebView*)webView addMessageToConsole:(NSDictionary*)message
 {
 	if (![message isKindOfClass:[NSDictionary class]]) { 
 		return;
 	}
 	
-	NSLog(@"JavaScript error: %@:%@: %@", 
+	NSLog(@"JavaScript console: %@:%@: %@", 
 		  [[message objectForKey:@"sourceURL"] lastPathComponent],	// could be nil
 		  [message objectForKey:@"lineNumber"],
 		  [message objectForKey:@"message"]);
 }
 
+- (NSArray *)webView:(WebView *)sender contextMenuItemsForElement:(NSDictionary *)element defaultMenuItems:(NSArray *)defaultMenuItems 
+{
+    NSMutableArray *webViewMenuItems = [[defaultMenuItems mutableCopy] autorelease];
+    
+    if (webViewMenuItems)
+    {
+        NSEnumerator *itemEnumerator = [defaultMenuItems objectEnumerator];
+        NSMenuItem *menuItem = nil;
+        while ((menuItem = [itemEnumerator nextObject]))
+        {
+            NSInteger tag = [menuItem tag];
+            
+            switch (tag)
+            {
+                case WebMenuItemTagOpenLinkInNewWindow:
+                case WebMenuItemTagDownloadLinkToDisk:
+                case WebMenuItemTagCopyLinkToClipboard:
+                case WebMenuItemTagOpenImageInNewWindow:
+                case WebMenuItemTagDownloadImageToDisk:
+                case WebMenuItemTagCopyImageToClipboard:
+                case WebMenuItemTagOpenFrameInNewWindow:
+                case WebMenuItemTagGoBack:
+                case WebMenuItemTagGoForward:
+                case WebMenuItemTagStop:
+                case WebMenuItemTagOpenWithDefaultApplication:
+                case WebMenuItemTagReload:
+                    [webViewMenuItems removeObjectIdenticalTo: menuItem];
+            }
+        }
+    }
+    
+    return webViewMenuItems;
+}
+
 #pragma mark WebScripting protocol
 
-/* checks whether a selector is acceptable to be called from JavaScript */
 + (BOOL) isSelectorExcludedFromWebScript:(SEL)selector
 {
 	return YES;
 }
 
-// right now exclude all properties (eg keys)
 + (BOOL) isKeyExcludedFromWebScript:(const char*)name
 {
 	return YES;
